22df628143ba8609c048154579f512a9f0aeedd2
Rmevada <ravimevada89@gmail.com>
2022-12-12 11:52:33 +0530

    Create Square-pattern.py

---
+++ Square-pattern.py
@@ -0,0 +1,13 @@
+#  SQUARE PATTERN IN PYTHON PROGRAMMING
+
+
+n = int(input())  # give input
+i =1
+while i<= n: # while loop to repeat
+    j=1
+    while j<= n:
+        print('$',end='') # print what you want in output
+
+        j= j+1
+    print()
+    i = i +1
