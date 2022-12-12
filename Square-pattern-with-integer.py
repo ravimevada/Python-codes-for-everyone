d2ee1260a643ae6499e48fdaca2dad3bbef3042a
Rmevada <ravimevada89@gmail.com>
2022-12-12 11:56:06 +0530

    Create Square-pattern-with-integer.py

---
+++ Square-pattern-with-integer.py
@@ -0,0 +1,13 @@
+#  SQUARE PATTERN WITH INTERGER IN PYTHON PROGRAMMING
+
+
+n = int(input())  # give input
+i =1
+while i<= n: # while loop to repeat
+    j=1
+    while j<= n:
+        print(n,end='') # print what you want in output
+
+        j= j+1
+    print()
+    i = i +1
